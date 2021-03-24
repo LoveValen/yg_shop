78df06cf08a2e59152e88c12a6d048e2eb1236db
White <1462449970@qq.com>
2021-03-24 12:04:30 +0800

    配置网络请求 轮播图

--- main.js
+++ main.js
@@ -1,11 +1,40 @@
 import Vue from 'vue'
 import App from './App'
+// 按需导入 $http 对象
+import {
+	$http
+} from "@escook/request-miniprogram"
+
+// 可以把 $http 挂载到 uni 顶级对象之上，方便全局调用
+uni.$http = $http
+// 配置请求根路径
+$http.baseUrl = 'https://www.uinav.com'
+
+// 请求开始之前做一些事情
+$http.beforeRequest = function(options) {
+	uni.showLoading({
+		title: '数据加载中...',
+	})
+}
+
+// 请求完成之后做一些事情
+$http.afterRequest = function() {
+	uni.hideLoading()
+}
+
+uni.$showMsg = function(title = '数据加载成功！', duration = 1500) {
+	uni.showToast({
+		title,
+		duration,
+		icon: 'none'
+	})
+}
 
 Vue.config.productionTip = false
 
 App.mpType = 'app'
 
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,12 @@
 <template>
 	<view>
-		
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
+			<swiper-item v-for="(item,index) in swiperList" :key="index">
+				<view class="swiper-item">
+					<image :src="item.image_src" mode=""></image>
+				</view>
+			</swiper-item>
+		</swiper>
 	</view>
 </template>
 
@@ -8,12 +14,33 @@
 	export default {
 		data() {
 			return {
-				
+				swiperList: []
 			};
+		},
+		onLoad() {
+			this.getSwiperList()
+		},
+		methods: {
+			// 获取轮播图数据
+			async getSwiperList() {
+				const {
+					data
+				} = await uni.$http.get('/api/public/v1/home/swiperdata')
+				if (data.meta.status !== 200) return uni.$showMsg('数据加载失败')
+				this.swiperList = data.message
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+	swiper {
+		height: 330rpx;
 
+		.swiper-item,
+		image {
+			width: 100%;
+			height: 100%;
+		}
+	}
 </style>
