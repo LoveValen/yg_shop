71209276a5ca2e9e2258d8e8523a355bfc34101c
White <1462449970@qq.com>
2021-03-24 15:07:28 +0800

    首页 分类导航区域

--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,5 +1,6 @@
 <template>
 	<view>
+		<!-- 首页轮播图 -->
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000">
 			<swiper-item v-for="(item,index) in swiperList" :key="index">
 				<view class="swiper-item">
@@ -7,6 +8,12 @@
 				</view>
 			</swiper-item>
 		</swiper>
+		<!-- 导航栏 -->
+		<view class="nav_list">
+			<view v-for="(item,index) in navList" :key="index" @click="navClickHanlder(item)">
+				<image :src="item.image_src" mode=""></image>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -14,20 +21,37 @@
 	export default {
 		data() {
 			return {
-				swiperList: []
+				swiperList: [],
+				navList: []
 			};
 		},
 		onLoad() {
 			this.getSwiperList()
+			this.getNavList()
 		},
 		methods: {
+			navClickHanlder(item) {
+				if (item.name === '分类') {
+					uni.switchTab({
+						url: '/pages/cate/cate'
+					})
+				}
+			},
 			// 获取轮播图数据
 			async getSwiperList() {
 				const {
 					data
 				} = await uni.$http.get('/api/public/v1/home/swiperdata')
-				if (data.meta.status !== 200) return uni.$showMsg('数据加载失败')
+				if (data.meta.status !== 200) return uni.$showMsg('加载失败')
 				this.swiperList = data.message
+			},
+			// 获取分类导航页面
+			async getNavList() {
+				const {
+					data
+				} = await uni.$http.get('/api/public/v1/home/catitems')
+				if (data.meta.status !== 200) return uni.$showMsg('加载失败')
+				this.navList = data.message
 			}
 		}
 	}
@@ -35,12 +59,25 @@
 
 <style lang="scss">
 	swiper {
-		height: 330rpx;
+		height: 340rpx;
 
 		.swiper-item,
 		image {
 			width: 100%;
 			height: 100%;
+		}
+	}
+
+	.nav_list {
+		display: flex;
+		justify-content: space-around;
+		margin: 15rpx 0;
+
+		view {
+			image {
+				width: 140rpx;
+				height: 150rpx;
+			}
 		}
 	}
 </style>
