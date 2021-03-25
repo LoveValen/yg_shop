1fe52bee60a784e0f05e7a4c1ed80f48f6172f79
White <1462449970@qq.com>
2021-03-25 18:24:37 +0800

    商品详情的UI结构渲染

--- subpkg/goods_detail/goods_detail.vue
+++ subpkg/goods_detail/goods_detail.vue
@@ -1,10 +1,23 @@
 <template>
 	<view>
 		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
-			<swiper-item v-for="(pic,index) in goodsInfo.picture">
-					<image :src="pic.pics_big" mode="widthFix" @click="preview(index)"></image>
+			<swiper-item v-for="(pic,index) in goodsInfo.pics">
+				<image :src="pic.pics_big" mode="widthFix" @click="preview(index)"></image>
 			</swiper-item>
 		</swiper>
+		<view class="goods_des" v-if="goods_info.goods_name">
+			<view class="goods_price">￥{{goodsInfo.goods_price}}</view>
+			<view class="des_middle">
+				<view class="goods_name">{{goodsInfo.goods_name}}</view>
+				<view class="goods_collect">
+					<uni-icons type="star" size="18" color="gray"></uni-icons>
+					<text>收藏</text>
+				</view>
+			</view>
+			<!-- 快递，免运费 -->
+			<view class="goods_freight">快递：免运费</view>
+		</view>
+		<rich-text :nodes="goodsInfo.goods_introduce"></rich-text>
 	</view>
 </template>
 
@@ -12,9 +25,7 @@
 	export default {
 		data() {
 			return {
-				goodsInfo:{
-					picture:[]
-				}
+				goodsInfo: {}
 			};
 		},
 		onLoad(options) {
@@ -22,19 +33,25 @@
 		},
 		methods: {
 			async getGoodsDetail(goods_id) {
-				const {data:{message,meta}} = await uni.$http.get('/api/public/v1/goods/detail', {
+				const {
+					data: {
+						message,
+						meta
+					}
+				} = await uni.$http.get('/api/public/v1/goods/detail', {
 					goods_id
 				})
-				if(meta.status !== 200) return uni.$showMsg()
-				this.goodsInfo.picture = message.pics
+				if (meta.status !== 200) return uni.$showMsg()
+				message.goods_introduce = message.goods_introduce.replace(/<img/g, '<img style="display:block"')
+				this.goodsInfo = message
 				console.log(message)
 			},
-			preview(index){
+			preview(index) {
 				uni.previewImage({
 					// 预览时，默认显示图片的索引
-					current:index,
+					current: index,
 					// 所有图片 url 地址的数组
-					urls: this.goodsInfo.picture.map(item=>item.pics_big)
+					urls: this.goodsInfo.pics.map(item => item.pics_big)
 				})
 			}
 		}
@@ -42,11 +59,47 @@
 </script>
 
 <style lang="scss">
-swiper{
-	width: 750rpx;
-	image{
-		width: 100%;
-		height: 100%;
+	swiper {
+		width: 750rpx;
+
+		image {
+			width: 100%;
+			height: 100%;
+		}
 	}
-}
+
+	.goods_des {
+		display: flex;
+		flex-direction: column;
+		padding: 10px;
+
+		.goods_price {
+			color: #c00000;
+			font-size: 20px;
+		}
+
+		.des_middle {
+			display: flex;
+
+			.goods_name {
+				padding: 10px 0;
+				padding-right: 10rpx;
+				border-right: 1px solid #efefef;
+			}
+
+			.goods_collect {
+				width: 320rpx;
+				padding: 16px 0;
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				align-items: center;
+			}
+		}
+
+		.goods_freight {
+			font-size: 12px;
+			color: #b6b6b6;
+		}
+	}
 </style>
