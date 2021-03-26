1dda5fcb4adbbed0b9da515366ac49398f9eb50c
White <1462449970@qq.com>
2021-03-26 16:43:35 +0800

    深度监听minixs中的total

--- mixins/tabbar-badge.js
+++ mixins/tabbar-badge.js
@@ -1,14 +1,24 @@
 import {
-		mapGetters
-	} from 'vuex'
-	export default {
-		computed: {
-			...mapGetters('cart', ['total'])
-		},
-		onShow() {
+	mapGetters
+} from 'vuex'
+export default {
+	computed: {
+		...mapGetters('cart', ['total'])
+	},
+	onShow() {
+		this.setBadge()
+	},
+	methods: {
+		setBadge() {
 			uni.setTabBarBadge({
 				index: 2, // tarBar索引
 				text: this.total + '' // 注意：text的值必须是字符串，不能是数字
 			})
 		}
+	},
+	watch: {
+		total() {
+			this.setBadge()
+		}
 	}
+}
